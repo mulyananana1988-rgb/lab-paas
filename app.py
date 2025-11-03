5a07cb256a20a6c1b1546421045f2b80c113646e
Nana Mulyana <mulyana.nana1988@gmail.com>
2025-11-03 09:34:08 +0700

    Update Lab Flask

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hellow Lab Flask"
+    return "Hello Lab Flask, saya Nana!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
